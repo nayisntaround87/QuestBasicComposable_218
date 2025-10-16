06096a7fc1d3651c2cc0dd55523399b4ce9433a0
nayisntaround87 <naylasalwahayati@gmail.com>
2025-10-16 14:25:07 +0700

    menambahkan kurung kurawal

--- app/src/main/java/com/example/layout/coba.kt
+++ app/src/main/java/com/example/layout/coba.kt
@@ -69,7 +69,9 @@ fun  TataletakColumnRow(modifier: Modifier) {
         ) {
             Text(text = "Komponen1Baris2")
             Text(text = "Komponen2Baris2")
+            Text(text = "Komponen3Baris2")
         }
-
-
     }
+
+
+}
