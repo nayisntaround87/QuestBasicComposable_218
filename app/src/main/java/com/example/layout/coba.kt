d524564484aef4feaab4720318266d27d84ec1bd
nayisntaround87 <naylasalwahayati@gmail.com>
2025-10-16 13:43:59 +0700

    menambahkan fillmaxwidth

--- app/src/main/java/com/example/layout/coba.kt
+++ app/src/main/java/com/example/layout/coba.kt
@@ -1,6 +1,8 @@
 package com.example.layout
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +21,8 @@ fun TataletakColumn(modifier : Modifier) {
 
 @Composable
 fun TataletakRow(modifier: Modifier) {
+    Row(modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = {  }
 
 }
 
