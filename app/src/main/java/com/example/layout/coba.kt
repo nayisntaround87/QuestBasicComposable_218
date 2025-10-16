73ac724190393a9391c99424d37fdd4d1d69033e
nayisntaround87 <naylasalwahayati@gmail.com>
2025-10-16 14:20:43 +0700

    membuat baris ke 2 di tataletakcolumnrow

--- app/src/main/java/com/example/layout/coba.kt
+++ app/src/main/java/com/example/layout/coba.kt
@@ -56,5 +56,11 @@ fun  TataletakColumnRow(modifier: Modifier) {
         // Baris 1
         Row(modifier = modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Baris1")
+            Text(text = "Komponen2Baris1")
+            Text(text = "Komponen3Baris1")
+        }
+        // Baris 2
+
 
         }
