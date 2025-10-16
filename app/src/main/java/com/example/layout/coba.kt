572ed87c75f25a38633dcc388618ea1aeb4284e6
nayisntaround87 <naylasalwahayati@gmail.com>
2025-10-16 13:54:13 +0700

    menambahkan alignment.center

--- app/src/main/java/com/example/layout/coba.kt
+++ app/src/main/java/com/example/layout/coba.kt
@@ -1,12 +1,15 @@
 package com.example.layout
 
+import android.text.Layout
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -34,7 +37,9 @@ fun TataletakRow(modifier: Modifier) {
 @Composable
 fun TataletakBox(modifier: Modifier) {
     Box(
-
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(), contentAlignment = Alignment.Center
     )
 
 }
