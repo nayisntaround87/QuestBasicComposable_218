3c8b32f9bb72083614fb5f608ad55a39c0654237
nayisntaround87 <naylasalwahayati@gmail.com>
2025-10-16 14:59:56 +0700

    menambahkan text

--- app/src/main/java/com/example/layout/coba.kt
+++ app/src/main/java/com/example/layout/coba.kt
@@ -20,7 +20,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TataletakColumn(modifier : Modifier) {
@@ -123,6 +126,14 @@ fun TataLetakBoxColumnRow(modifier: Modifier) {
             Image(painter = gambar,
                 contentDescription = null,
                 contentScale = ContentScale.Fit)
+            Text(text = "My Music"),
+                fontSize = 50.sp,
+                color = Color.Red
+                fontWeight = FontWeight.Bold
+                fontFamily = FontFamily.Cursive
+                modifier= Modifier.align(
+                    alignment = Alignment.Center))
+                )
 
         }
     }
