f6d99c846da96713d6d161b4cbc1be8570cab56b
nayisntaround87 <naylasalwahayati@gmail.com>
2025-10-16 14:34:18 +0700

    menambahkan color yellow

--- app/src/main/java/com/example/layout/coba.kt
+++ app/src/main/java/com/example/layout/coba.kt
@@ -1,6 +1,7 @@
 package com.example.layout
 
 import android.text.Layout
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
@@ -84,6 +86,7 @@ fun TataLetakBoxColumnRow(modifier: Modifier) {
             modifier = modifier
                 .fillMaxWidth()
                 .height(110.dp)
+                .background(color = Color.Yellow)
         )
     }
 }
